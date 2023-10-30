6a01214881ee3c3b3e7476d789287090ec6ad0c8
andraskapitany <andras_kapitany@hotmail.hu>
2023-10-30 23:15:15 +0100

    baseic

---
+++ src/test/java/hu/progmatic/battleship_torpedotigrisek/BattleshipTorpedoTigrisekApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmatic.battleship_torpedotigrisek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipTorpedoTigrisekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
