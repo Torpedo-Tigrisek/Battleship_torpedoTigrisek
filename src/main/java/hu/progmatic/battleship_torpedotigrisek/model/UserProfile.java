83a874ad65506bb94870338cbb6ac3ec2b878865
Krisz6 <szalay.krisztian93@gmail.com>
2023-11-02 19:47:48 +0100

    mysql txt, rossz modellek

---
+++ src/main/java/hu/progmatic/battleship_torpedotigrisek/model/UserProfile.java
@@ -0,0 +1,25 @@
+package hu.progmatic.battleship_torpedotigrisek.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private int score;
+    private int wins;
+    private int losses;
+    private double winLossRatio;
+}
