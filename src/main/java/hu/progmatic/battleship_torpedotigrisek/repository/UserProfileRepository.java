180be8ca8d627798bc2740a2e8445c0dc7b604e5
vrena4567 <v.rena4567@gmail.com>
2023-11-04 11:15:59 +0100

    database debugged

--- src/main/java/hu/progmatic/battleship_torpedotigrisek/repository/UserProfileRepository.java
+++ src/main/java/hu/progmatic/battleship_torpedotigrisek/repository/UserProfileRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserProfileRepository extends JpaRepository<User, Integer> {
+public interface UserProfileRepository extends JpaRepository<User, Long> {
     User findByName(String userName);
 }
