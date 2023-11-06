9893430d8551ae2a19f5ddc89dfb36471cf9365a
andraskapi <andras_kapitany@hotmail.hu>
2023-11-06 18:49:50 +0100

    ship class basic done

--- src/main/java/hu/progmatic/battleship_torpedotigrisek/model/Ship.java
+++ src/main/java/hu/progmatic/battleship_torpedotigrisek/model/Ship.java
@@ -1,7 +1,59 @@
 package hu.progmatic.battleship_torpedotigrisek.model;
 
+
+
+
 public class Ship {
 
-    private String shipName;
+    public enum ShipNameList {
+        CARRIER, CRUISER, SUBMARINE, DESTROYER, ATTACKER
+    }
 
+    private ShipNameList shipName;
+    private int size;
+    private int[] coordinate;
+    private int countHit;
+
+
+    public Ship (int size, ShipNameList shipName) {
+        this.size = size;
+        this.countHit = 0;
+        coordinate = new int[size];
+        for (int i = 0; i<size; i++) {
+            coordinate[i] = 0;
+        }
+        this.shipName = shipName;
+    }
+
+    public ShipNameList getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(ShipNameList shipName) {
+        this.shipName = shipName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int[] getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(int[] coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public int getCountHit() {
+        return countHit;
+    }
+
+    public void setCountHit(int countHit) {
+        this.countHit = countHit;
+    }
 }
