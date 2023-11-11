ce4f9874f156708ad146cbe8ac8b01a6edc82ea6
Kiscica79 <oroszildiko79@gmail.hu>
2023-11-11 11:39:29 +0100

    display_game.html and style.css commit and push

---
+++ src/main/java/hu/progmatic/battleship_torpedotigrisek/controller/UserProfileController.java
@@ -0,0 +1,25 @@
+
+package hu.progmatic.battleship_torpedotigrisek.controller;
+
+
+import hu.progmatic.battleship_torpedotigrisek.model.User;
+import hu.progmatic.battleship_torpedotigrisek.service.UserProfileService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class UserProfileController {
+
+    private UserProfileService userProfileService;
+
+    @GetMapping("/profile")
+    public String userProfile(Model model, Principal principal) {
+        String userName = principal.getName();
+        User user = userProfileService.getUserProfileByName(userName);
+        model.addAttribute("user", user);
+        return "html/profile";
+    }
+}
