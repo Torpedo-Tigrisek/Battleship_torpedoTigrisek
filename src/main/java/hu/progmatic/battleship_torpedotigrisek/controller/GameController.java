c93656dcc3ffa0db60b1820a8d76ca414c23d037
Kiscica79 <oroszildiko79@gmail.hu>
2023-11-11 11:39:29 +0100

    display_game.html and style.css commit and push

---
+++ src/main/java/hu/progmatic/battleship_torpedotigrisek/controller/GameController.java
@@ -0,0 +1,10 @@
+package hu.progmatic.battleship_torpedotigrisek.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class GameController {
+    
+}
