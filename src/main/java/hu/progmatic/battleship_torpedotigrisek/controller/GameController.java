5cc7e53e07e51d1ac5f3113ace2f31afcf1a49af
andraskapi <andras_kapitany@hotmail.hu>
2023-11-13 20:00:16 +0100

    dummy boat on the board

--- src/main/java/hu/progmatic/battleship_torpedotigrisek/controller/GameController.java
+++ src/main/java/hu/progmatic/battleship_torpedotigrisek/controller/GameController.java
@@ -6,16 +6,21 @@ import hu.progmatic.battleship_torpedotigrisek.model.Ship;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GameController {
 
+    private Board playerBoard = new Board();
+
+    /*
     @GetMapping("/testBoard")
     public String getTestBoard(Model model) {
         Board myBoard = new Board();
         Board opponentBoard = new Board();
 
-        Ship myShip = new Ship(4, 0, 0);
+        Ship myShip = new Ship(0, 0, 0);
         myBoard.placeShip(myShip);
 
         model.addAttribute("myGrid", myBoard.getGrid());
@@ -23,9 +28,24 @@ public class GameController {
 
         return "test-board";
     }
+    */
+
 
     @GetMapping("/dinamicboard")
     public String getDinamycBoard(){
         return "dinamichtml1";
+    }
+
+    @GetMapping("/testBoard")
+    public String gameBoard(Model model) {
+        model.addAttribute("board", playerBoard);
+        return "test-board";
+    }
+
+    @PostMapping("/placeShip")
+    public String placeShip(Ship ship, Model model) {
+        playerBoard.placeShip(ship);
+        System.out.println("Hajó elhelyezve: " + ship);
+        return "redirect:/testBoard";
     }
 }
