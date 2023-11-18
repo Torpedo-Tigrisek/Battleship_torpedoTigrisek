1edb29487885ec5ba3ab2070b49d2a8073f2645e
andraskapi <andras_kapitany@hotmail.hu>
2023-11-18 11:43:29 +0100

    websocket, ship model, dummy shot

--- src/main/java/hu/progmatic/battleship_torpedotigrisek/controller/GameController.java
+++ src/main/java/hu/progmatic/battleship_torpedotigrisek/controller/GameController.java
@@ -19,23 +19,6 @@ public class GameController {
     private List<Ship> ships = new ArrayList<>();
 
 
-    /*
-    @GetMapping("/testBoard")
-    public String getTestBoard(Model model) {
-        Board myBoard = new Board();
-        Board opponentBoard = new Board();
-
-        Ship myShip = new Ship(0, 0, 0);
-        myBoard.placeShip(myShip);
-
-        model.addAttribute("myGrid", myBoard.getGrid());
-        model.addAttribute("opponentGrid", opponentBoard.getGrid());
-
-        return "test-board";
-    }
-    */
-
-
     @GetMapping("/dinamicboard")
     public String getDinamycBoard(){
         return "dinamichtml1";
@@ -48,43 +31,7 @@ public class GameController {
 
         return "test-board";
     }
-/*
-    @PostMapping("/placeShip")
-    public String placeShip(Ship ship) {
 
-        playerBoard.placeShip(ship);
-        System.out.println("Location: " + ship);
-        ships.add(ship);
-        System.out.println(ships);
-
-        return "redirect:/testBoard";
-    }
-
- */
-
-    /*
-    @PostMapping("/placeShip")
-    public String placeShip(@RequestParam ShipType shipType,
-                            @RequestParam int startX,
-                            @RequestParam int startY,
-                            @RequestParam String orientation) {
-        // Létrehoz egy új hajót a felhasználó által megadott paraméterekkel
-        Ship newShip = new Ship(shipType, startX, startY, orientation);
-
-        // Hozzáadja a hajót a táblához
-        playerBoard.addShip(newShip);
-
-        // Elhelyezi a hajót a táblán
-        playerBoard.placeShip(newShip);
-        ships.add(newShip);
-        System.out.println("Location: " + newShip);
-        System.out.println(ships);
-
-
-        return "redirect:/testBoard";
-    }
-
-     */
 
 
 }
